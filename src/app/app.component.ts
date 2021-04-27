7b6d594f1af3bdd755fe6cd7abe3ee4dfe2228c2
Rafael Neves <rafaelneves652@gmail.com>
2021-04-26 21:40:32 -0300

    rectified images format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  images: string[] = [
+    'assets/images/chegaDeDor.webp',
+    'assets/images/alimentacao.webp',
+    'assets/images/atividadeFisica.webp',
+    'assets/images/valvulaEscape.webp',
+    'assets/images/Sono.webp',
+    'assets/images/oqVcAcha.webp'
+  ];
+
   customOptions: OwlOptions = {
     loop: true,
     margin: 10,
@@ -17,15 +27,20 @@ export class AppComponent {
     navText: ['&#8249', '&#8250;'],
     responsive: {
       0: {
-        items: 1
+        items: 2,
       },
-      400: {
-        items: 2
+      600: {
+        items: 3,
       },
       1000: {
-        items: 3
+        items: 5,
       }
     },
+  }
 
+  ngOnInit() {
+    for (let i = 0; i < this.images.length; i++) {
+      let image = this.images[i];
+    }
   }
 }
